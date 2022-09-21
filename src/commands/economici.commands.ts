211b73cb32eab4c5af1d7ec2437517debb408239
nf <nicholas.facchin@finconsgroup.com>
2022-09-21 17:39:37 +0200

    debug

--- src/commands/economici.commands.ts
+++ src/commands/economici.commands.ts
@@ -5,26 +5,31 @@ import { CurrentCtx } from "../interfaces/context.models";
 import haversine from "haversine-distance";
 
 export const economiciCommand = async (ctx: CurrentCtx) => {
+  console.log("arrivo qui?");
   if (!ctx?.session?.carburanti) {
+    console.log("arrivo qui1?");
     await ctx?.reply(
       "Sembra che tu non abbia impostato i filtri per carburante!"
     );
     return ctx?.scene?.leave();
   } else if (!ctx?.session?.posizione) {
+    console.log("arrivo qui2?");
     await ctx?.reply(
       `Sembra che tu non abbia impostato la posizione di ricerca!\nUsa il comando /${CommandsEnum.IMPOSTA_POSIZIONE} per impostare la tua posizione`
     );
     return ctx?.scene?.leave();
   } else {
+    console.log("arrivo qui3?");
     const distributori = await getDistributoriVicini(
       ctx.session.carburanti,
       ctx.session.posizione
     );
-
+    console.log("arrivo qui4?");
     const links = distributori.map(
       (distributore) =>
         `(https://www.google.com/maps/search/?api=1&query=${distributore.location.lat},${distributore.location.lng})`
     );
+    console.log("arrivo qui5?");
     const { lat, long } = ctx.session.posizione;
     const distributoriText = distributori.reduce((acc, val, index) => {
       const { lat: lat2, lng: long2 } = val.location;
@@ -63,6 +68,7 @@ export const economiciCommand = async (ctx: CurrentCtx) => {
         carburanteText = key;
       }
     });
+    console.log("arrivo qui6?");
     await ctx.reply(
       `Ecco a te i distributori di ${carburanteText} più economici vicino a ${ctx.session.posizione.description}:`
     );
